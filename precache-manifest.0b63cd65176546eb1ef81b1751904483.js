7c07246c98551c6134d41c46531aaa461b7ffecd
Ashok <ashok@emadri.com>
2020-03-03 12:09:06 +0530

    Updates

---
+++ precache-manifest.0b63cd65176546eb1ef81b1751904483.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89fa15df174041dcbd20f0572d2f7e2c",
+    "url": "/react-store/index.html"
+  },
+  {
+    "revision": "257c04e682ae7d8a5f95",
+    "url": "/react-store/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "f55d23262152da3468a5",
+    "url": "/react-store/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "257c04e682ae7d8a5f95",
+    "url": "/react-store/static/js/2.a8554c74.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react-store/static/js/2.a8554c74.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f55d23262152da3468a5",
+    "url": "/react-store/static/js/main.df64e521.chunk.js"
+  },
+  {
+    "revision": "eba475d003aab241a3cb",
+    "url": "/react-store/static/js/runtime-main.38818931.js"
+  }
+]);
